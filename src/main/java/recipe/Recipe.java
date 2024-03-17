fe89f02645e68dc786a78a9348b983e9e4b975e9
Ewert, Christopher - D0306580 <christopher.ewert@dm.de>
2024-03-17 16:24:52 +0100

    Add class Recipe

---
+++ src/main/java/recipe/Recipe.java
@@ -0,0 +1,32 @@
+package recipe;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+public record Recipe(
+        String name,
+        String author,
+        LocalDate dateOfCreation,
+        LocalDateTime lastUpdated,
+        String content,
+        List<String> categories,
+        LocalTime cookingTime,
+        int rating
+) {
+    public Recipe() {
+        this("Recipe name", "Author", LocalDate.now(), null, "Recipe content", List.of(), LocalTime.now(), 0);
+    }
+
+    public Recipe(String name, String author, LocalDate dateOfCreation, LocalDateTime lastUpdated, String content, List<String> categories, LocalTime cookingTime, int rating) {
+        this.name = name;
+        this.author = author;
+        this.dateOfCreation = dateOfCreation;
+        this.lastUpdated = lastUpdated;
+        this.content = content;
+        this.categories = categories;
+        this.cookingTime = cookingTime;
+        this.rating = rating;
+    }
+}
