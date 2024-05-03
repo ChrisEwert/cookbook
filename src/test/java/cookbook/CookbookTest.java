2fb1b26acf3b7f0c07cb4fc9dc3830ed5e1502b0
Ewert, Christopher - D0306580 <christopher.ewert@dm.de>
2024-05-03 18:49:39 +0200

    Add CookbookTest

--- src/test/java/cookbook/CookbookTest.java
+++ src/test/java/cookbook/CookbookTest.java
@@ -2,11 +2,25 @@ package cookbook;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class CookbookTest {
 
     @Test
     void changeCurrentUsername() {
+        // ARRANGE
+        Cookbook cookbook = new Cookbook();
+
+        String username = "New username";
+
+        // ACT
+        Cookbook updatedCookbook = cookbook.changeCurrentUsername(username);
+
+        // ASSERT
+        assertThat(updatedCookbook.dateOfCreation())
+            .isEqualTo(cookbook.dateOfCreation());
+        assertThat(updatedCookbook.currentUsername())
+            .isNotEqualTo(cookbook.currentUsername())
+            .isEqualTo(username);
     }
 }
