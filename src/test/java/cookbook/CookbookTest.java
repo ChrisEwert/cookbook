0b5429ea864ecd50a457a06ce01ca26e60f27282
Ewert, Christopher - D0306580 <christopher.ewert@dm.de>
2024-05-03 18:45:18 +0200

    Add RecipeTest

---
+++ src/test/java/cookbook/CookbookTest.java
@@ -0,0 +1,12 @@
+package cookbook;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CookbookTest {
+
+    @Test
+    void changeCurrentUsername() {
+    }
+}
